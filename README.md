bfcda6c3919653976c27836bdd1f6d39c696b733
Navnathjagtap2311 <navnathjagtap2311@gmail.com>
2023-12-03 01:38:41 +0530

    added webdriver manager and other test cases

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # DockerSelenium
+# ------------------
+# Orphaned comments:
+
+#http://localhost:4444/ui
+#with yml start-> docker-compose up -d
+#end->docker-compose down
+
+#docker version-> docker -v
+#docker imeges installed--> docker images 
+#docker pull selenium/standalone-chrome:latest
+#running docker container->docker ps
+# 4444 port is good for both browser
+# ------------------
+#without yml file
+# docker run -d -p 4444:4444 selenium/standalone-chrome
+#docker run -d -p 4444:4444 selenium/standalone-firefox
+#  no need  docker run -d -p 4444:4444 -p 7900:7900 selenium/standalone-chrome
+#for firefox--> mvn test -Dbrowser=firefox
--- src/test/java/webDriverManager/GoogleSearchUsingWebDriverManager.java
+++ src/test/java/webDriverManager/GoogleSearchUsingWebDriverManager.java
@@ -1,35 +1,36 @@
-package WithOutDocker;
+package webDriverManager; 
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-public class GoogleSearchLocalSystem {
+public class GoogleSearchUsingWebDriverManager {
 
 	public static void main(String[] args) {
-		System.out.println("-----for firefox change the org.seleniumhq.selenium to 4.15.0-------");
+		System.out.println("-----for firefox change the org.seleniumhq.selenium to 4.15.0 for chrome-------");
 		testGoogleSearch("chrome");
-//		System.out.println("-----for firefox change the org.seleniumhq.selenium to 4.1.0-------");
+//		System.out.println("-----for firefox change the org.seleniumhq.selenium to 4.1.0 for firefox-------");
 //        testGoogleSearch("firefox");
 	}
 
 	public static void testGoogleSearch(String browser) {
 		WebDriver driver;
+		
 		if ("chrome".equalsIgnoreCase(browser)) {
 			System.out.println("----chrome---");
-			System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe");
-			driver = new ChromeDriver();
+			WebDriverManager.chromedriver().setup();
+			ChromeOptions chromeOptions = new ChromeOptions();
+			driver = new ChromeDriver(chromeOptions);
 		} else if ("firefox".equalsIgnoreCase(browser)) {
 			System.out.println("----firefox---");
-			System.setProperty("webdriver.gecko.driver", "src/test/resources/drivers/geckodriver.exe");
-			FirefoxOptions firefoxOptions = new FirefoxOptions();
-			driver = new FirefoxDriver(firefoxOptions);
+			WebDriverManager.firefoxdriver().setup();	
+			driver = new FirefoxDriver();
 		} else {
 			throw new IllegalArgumentException("Invalid browser name: " + browser);
 		}
+
 		try {
 			System.out.println("------stared test case---------");
 			driver.get("https://www.google.com");
